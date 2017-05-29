7d3631e9bfebb4a6a5647a131afa7edbc8f0d503
nanako <o.o.nanako.o.o@gmail.com>
2017-05-30 01:04:34 +0900

    Initial Commit

---
+++ test4/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  test4
+//
+//  Created by nanako on 2017/05/30.
+//  Copyright © 2017年 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let fruits = ["リンゴ", "みかん", "ぶどう"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /// セルの個数を指定するデリゲートメソッド（必須）
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruits.count
+    }
+    
+    /// セルに値を設定するデータソースメソッド（必須）
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        // セルを取得する
+        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
+        
+        // セルに表示する値を設定する
+        cell.textLabel!.text = fruits[indexPath.row]
+        
+        return cell
+    }
+    
+    /// セルが選択された時に呼ばれるデリゲートメソッド
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("セル番号：\(indexPath.row) セルの内容：\(fruits[indexPath.row])")
+    }
+}
