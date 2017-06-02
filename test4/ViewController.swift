e52d4ac09d2fa03f1b8853f1a542e60bfd8129d7
nanako <o.o.nanako.o.o@gmail.com>
2017-06-02 17:27:33 +0900

    曜日と時限をいれた

--- test4/ViewController.swift
+++ test4/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    let fruits = ["リンゴ", "みかん", "ぶどう"]
+    let fruits = ["月曜1限","月曜2限","月曜3限","月曜4限","月曜5限","月曜6限",
+        "火曜1限","火曜2限","火曜3限","火曜4限","火曜5限","火曜6限",
+                     "水曜1限","水曜2限","水曜3限","水曜4限","水曜5限","水曜6限","木曜1限","木曜2限","木曜3限","木曜4限","木曜5限","木曜6限","金曜1限","金曜2限","金曜3限","金曜4限","金曜5限","金曜6限"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
