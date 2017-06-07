a143cf15a69c4fb3a153dabdc5b5dc00417cc7d2
nanako <o.o.nanako.o.o@gmail.com>
2017-06-07 16:39:54 +0900

    配列作成中

--- test4/secondViewController.swift
+++ test4/secondViewController.swift
@@ -16,13 +16,16 @@ class secondViewController: UIViewController,UITextFieldDelegate{
     var attend : Int = 0
     var absent : Int = 0
     
-     let userDefaults = UserDefaults.standard
+    let userDefaults = UserDefaults.standard
     
-            override func didReceiveMemoryWarning() {
+    var absentrArray: [String] = []
+    var attendrArray: [String] = []
+    
+    override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-            
-             }
+        
+    }
     @IBAction func Attendance(){
         attend = attend + 1
         AttendLabel.text = String(attend)
@@ -31,29 +34,33 @@ class secondViewController: UIViewController,UITextFieldDelegate{
         absent = absent + 1
         AbsentLabel.text = String(absent)
     }
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         //String型配列のデータを保存、読込
-        userDefaults.set([attend], forKey: "testStringArray")
-        let result6 = userDefaults.stringArray(forKey: "testStringArray")
+        userDefaults.set([attend], forKey: "attendrArray")
+        let result1 = userDefaults.stringArray(forKey: "attendrArray")
         
-        print("fluits:\(result6!)")
+        userDefaults.set([absent], forKey: "absentrArray")
+        let result2 = userDefaults.stringArray(forKey: "absentrArray")
+        
+         print("attendrArray:\(result1!)")
+        print("absentrArray:\(result2!)")
         
     }
-   
-
+    
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
