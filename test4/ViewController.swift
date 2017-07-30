0b15c1647473c93329a1e35ec1e4292608d0f072
nanako <o.o.nanako.o.o@gmail.com>
2017-07-31 00:25:50 +0900

    secondViewControllerのデザイン、アイコンデザイン済み

--- test4/ViewController.swift
+++ test4/ViewController.swift
@@ -10,16 +10,33 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
-    let fruits = ["月曜1限","月曜2限","月曜3限","月曜4限","月曜5限","月曜6限",
+    let defaultTitleArray = ["月曜1限","月曜2限","月曜3限","月曜4限","月曜5限","月曜6限",
         "火曜1限","火曜2限","火曜3限","火曜4限","火曜5限","火曜6限",
                      "水曜1限","水曜2限","水曜3限","水曜4限","水曜5限","水曜6限","木曜1限","木曜2限","木曜3限","木曜4限","木曜5限","木曜6限","金曜1限","金曜2限","金曜3限","金曜4限","金曜5限","金曜6限"]
-   
+
     
+   
+    let userDefaults = UserDefaults.standard
+    
+    @IBOutlet var tableView : UITableView!
+    
+    var titleArray: [String] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        titleArray = userDefaults.array(forKey: "title") as? [String] ??
+        defaultTitleArray
+        
+        tableView.reloadData()
+  
+    
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -27,7 +44,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     /// セルの個数を指定するデリゲートメソッド（必須）
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return fruits.count
+        return titleArray.count
     }
     
     /// セルに値を設定するデータソースメソッド（必須）
@@ -38,14 +55,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         // セルに表示する値を設定する
         
-        cell.textLabel!.text = fruits[indexPath.row]
+        cell.textLabel!.text = titleArray[indexPath.row]
         
         return cell
     }
     
     /// セルが選択された時に呼ばれるデリゲートメソッド
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("セル番号：\(indexPath.row) セルの内容：\(fruits[indexPath.row])")
+        print("セル番号：\(indexPath.row) セルの内容：\(titleArray[indexPath.row])")
         
         
         
--- test4/secondViewController.swift
+++ test4/secondViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class secondViewController: UIViewController, UITextViewDelegate{
+class secondViewController: UIViewController, UITextViewDelegate, UITextFieldDelegate{
     
     @IBOutlet var AttendLabel : UILabel!
     @IBOutlet var AbsentLabel: UILabel!
-    @IBOutlet weak var testTextView: UITextView!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var textField: UITextField!
     
     var attend : Int = 0
     var absent : Int = 0
@@ -21,15 +22,17 @@ class secondViewController: UIViewController, UITextViewDelegate{
     
     var attendArray: [Int] = []
     var absentArray: [Int] = []
+    var memoArray: [String] = []
+    var titleArray: [String] = []
     
     let defaultAttendArray = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     let defaultAbsentArray = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    let defaultMemoArray = ["","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""]
+    let defaultTitleArray = ["月曜1限","月曜2限","月曜3限","月曜4限","月曜5限","月曜6限",
+                             "火曜1限","火曜2限","火曜3限","火曜4限","火曜5限","火曜6限",
+                             "水曜1限","水曜2限","水曜3限","水曜4限","水曜5限","水曜6限","木曜1限","木曜2限","木曜3限","木曜4限","木曜5限","木曜6限","金曜1限","金曜2限","金曜3限","金曜4限","金曜5限","金曜6限"]
     
     var secondIndex = 0
-    
-    
-    // 7. SecondViewに渡す文字列
-    var selectedText: String?
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -49,8 +52,14 @@ class secondViewController: UIViewController, UITextViewDelegate{
         userDefaults.set(absentArray, forKey: "absent")
     }
     @IBAction func back(){
+        memoArray[secondIndex] = textView.text
+        userDefaults.set(memoArray, forKey: "memo")
+        titleArray[secondIndex] = textField.text!
+        userDefaults.set(titleArray, forKey: "title")
         dismiss(animated: true, completion: nil)
     }
+    
+
     
     
     override func viewDidLoad() {
@@ -59,33 +68,46 @@ class secondViewController: UIViewController, UITextViewDelegate{
         
         attendArray = userDefaults.array(forKey: "attend") as? [Int] ?? defaultAttendArray
         absentArray = userDefaults.array(forKey: "absent") as? [Int] ?? defaultAbsentArray
+        memoArray = userDefaults.array(forKey: "memo") as? [String] ??
+        defaultMemoArray
+        titleArray = userDefaults.array(forKey: "title") as? [String] ??
+        defaultTitleArray
         
         attend = attendArray[secondIndex]
         absent = absentArray[secondIndex]
         
         AttendLabel.text = String(attendArray[secondIndex])
         AbsentLabel.text = String(absentArray[secondIndex])
+        textView.text = String(memoArray[secondIndex])
+        textField.text = String(titleArray[secondIndex])
         
         //ビューを作成する。
         let testView = UIView()
-        
-        
+        testView.frame.size.height = 60
+        testView.backgroundColor = UIColor.blue
         //「閉じるボタン」を作成する。
+       
         
-        //ビューに「閉じるボタン」を追加する。
         
+    //ビューに「閉じるボタン」を追加する。
+        let closeButton = UIButton(frame: CGRect(x: UIScreen.main.bounds.size.width - 70 , y: 0, width: 70, height: 50))
+        closeButton.setTitle("閉じる", for: UIControlState.normal)
+        closeButton.backgroundColor = UIColor.red
+        closeButton.addTarget(self, action: #selector(onClickCloseButton(sender:)), for: .touchUpInside)
+        
+         testView.addSubview(closeButton)
         
         //キーボードのアクセサリにビューを設定する。
-        testTextView.inputAccessoryView = testView
+        textView.inputAccessoryView = testView
         
         //テキストビューのデリゲート先にこのインスタンスを設定する。
-        testTextView.delegate = self
+        textView.delegate = self
     }
     
     //「閉じるボタン」で呼び出されるメソッド
     func onClickCloseButton(sender: UIButton) {
         //キーボードを閉じる
-        testTextView.resignFirstResponder()
+        textView.resignFirstResponder()
     }
     
     // 電話番号
